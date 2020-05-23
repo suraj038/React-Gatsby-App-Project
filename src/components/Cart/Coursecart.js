dcb236acec9853fe47d8327457d100f3b64b5fc9
suraj038 <surajsingh98450@gmail.com>
2020-05-24 01:04:14 +0530

    added one more dollar sign

--- src/components/Cart/Coursecart.js
+++ src/components/Cart/Coursecart.js
@@ -26,7 +26,7 @@ export default class Coursecart extends Component {
                                         <div className="flex-grow-1 px-3">
                                             <div className="d-flex">
                                                 <h6 className="mb-0">{node.title}</h6>
-                                                <h6 className="mb-0 text-success ml-4">$ {node.price}</h6>
+                                                <h6 className="mb-0 text-success ml-4">$$ {node.price}</h6>
                                             </div>
                                             <p className="text-muted">
                                                 <small>{node.description.description}</small>
